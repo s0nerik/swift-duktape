67c43eb542075fda57c9d0082d80adcfdfd97075
Alex Isaienko <thesonerik@gmail.com>
2018-06-22 17:25:18 +0300

    Ported to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,17 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "swift-duktape",
-    exclude: [
-        "Upstream"
+    products: [
+        .library(
+            name: "swift-duktape",
+            targets: ["Duktape"]),
+    ],
+    dependencies: [],
+    targets: [
+		.target(name: "CDuktape", dependencies: []),
+		.target(name: "Duktape", dependencies: ["CDuktape"]),
     ]
 )
